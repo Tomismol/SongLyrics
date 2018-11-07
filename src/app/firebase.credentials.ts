14a070c5b65e97a602440c0d7ae1189fdba76e90
Tomismol <tom.smolarek01@gmail.com>
2018-11-07 14:58:15 +0200

    Initial Commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyALTzQNSuCUTP-H0KkGcfdji5oBYmeIFcE",
+    authDomain: "songlyrics-373e6.firebaseapp.com",
+    databaseURL: "https://songlyrics-373e6.firebaseio.com",
+    projectId: "songlyrics-373e6",
+    storageBucket: "songlyrics-373e6.appspot.com",
+    messagingSenderId: "552027697856"  
+};
